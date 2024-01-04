5170d073717f1331aa7092577ece953746910893
rakib54 <rakibur5454@gmail.com>
2024-01-04 15:39:39 +0600

    updated hosted api url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:9000'
+  baseURL: 'https://blogs-data-cs4r.onrender.com'
 });
 
 export default axiosInstance
